2efb49ebff71080c0f85d5f9f435951e3460c582
nicamanas <xyukimiku@gmail.com>
2023-03-10 01:50:58 +0800

    bug fixes
    fixed track invalid result

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom"
 
 function Login() {
 	const navigate = useNavigate()
-	const { user, setUser } = useContext(UserContext)
+	const { setUser } = useContext(UserContext)
 
 	function handleSubmit(e: any) {
 		e.preventDefault()
